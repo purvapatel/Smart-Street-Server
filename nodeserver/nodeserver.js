8f6f4cae96d98ce4dd451b732fdb5860a563d1da
purva5194 <purvapatel@Purvas-MacBook-Pro.local>
2017-10-22 23:01:51 -0700

    change in port

--- nodeserver/nodeserver.js
+++ nodeserver/nodeserver.js
@@ -35,5 +35,6 @@ app.post('/userlist', function (req, res) {
   });
 });
 
-app.listen(3000);
-console.log("Server running on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
